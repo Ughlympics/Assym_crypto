4f4a20ab3ff1bed8671b8ca5529b3f2193eb4565
Ughlympics <kipgora@gmail.com>
2025-10-31 15:29:13 +0200

    Some func of MR

--- Lab2/main.go
+++ Lab2/main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"crypto/rand"
 	"fmt"
+	"math/big"
 )
 
 func main() {
 	fmt.Println("This is Lab2. Please refer to the assignment instructions.")
+	var aStr2 = "425D2B9BFDB25B9CF6C416CC6E37B59C1F"
+	var a2, _ = new(big.Int).SetString(aStr2, 16)
+	fmt.Println("a2 byte length:", a2.BitLen())
+	r0, err := rand.Int(rand.Reader, a2)
+	if err != nil {
+		fmt.Println("Error generating random number:", err)
+		return
+	}
+	fmt.Println("Generated random number:", r0)
 }
--- Lab2/rsa/key_schedule/gen.go
+++ Lab2/rsa/key_schedule/gen.go
@@ -2,6 +2,7 @@ package rsa
 
 import (
 	"crypto/rand"
+	"fmt"
 	"math/big"
 )
 
@@ -12,7 +13,7 @@ var p2, _ = new(big.Int).SetString(pStr2, 16)
 var a2, _ = new(big.Int).SetString(aStr2, 16)
 var m = new(big.Int).Mul(p2, a2)
 
-func BBSGenerator_byte(n int) ([]byte, error) {
+func BBSGenerator_byte(n int) (*big.Int, error) {
 	r0, err := rand.Int(rand.Reader, p2)
 	if err != nil {
 		return nil, err
@@ -30,5 +31,35 @@ func BBSGenerator_byte(n int) ([]byte, error) {
 		bytes[i] = byte(mod256.Int64())
 	}
 
-	return bytes, nil
+	result := new(big.Int).SetBytes(bytes)
+	return result, nil
+}
+
+func MillerRabinTest(p *big.Int, k int) (bool, error) {
+	undp1 := new(big.Int).Sub(p, big.NewInt(1))
+	d, r := decomposition(undp1)
+	fmt.Println("d:", d)
+	fmt.Println("r:", r)
+	return true, nil
+}
+
+func decomposition(n *big.Int) (d *big.Int, r *big.Int) {
+	d = new(big.Int).Set(n)
+	r = big.NewInt(0)
+	for i := 0; ; i++ {
+		if new(big.Int).Mod(d, big.NewInt(2)).Cmp(big.NewInt(0)) != 0 {
+			return d, r
+		}
+		d.Div(d, big.NewInt(2))
+		r.Add(r, big.NewInt(1))
+	}
+}
+
+func step1(n *big.Int) (*big.Int, bool) {
+	k := a2.BitLen() / 8
+	randNum, err := BBSGenerator_byte(k)
+	if err != nil {
+		return nil, false
+	}
+	return randNum.Mod(randNum, n), true
 }
